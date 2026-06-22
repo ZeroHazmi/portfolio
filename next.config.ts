475e03dc404e6eedd28c3c7eabb7b9107ba81ab9
Zal Hazmi Bin Musrizal <zerohazmi@gmail.com>
2026-06-22 23:41:02 +0800

    Modify to deploy for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  output: "export",          // Generates static HTML in /out
-  basePath: isProd ? "/portfolio" : "",   // Sub-path on GitHub Pages
-  assetPrefix: isProd ? "/portfolio/" : "",
-  images: {
-    unoptimized: true,       // Required for static export (no image server)
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
